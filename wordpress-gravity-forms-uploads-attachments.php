ebae7fe3fd21c5199876c67c47424124ac8d565e
Takis Bouyouris <takis@nevma.gr>
2017-10-20 23:36:10 +0300

    Description only.

--- wordpress-gravity-forms-uploads-attachments.php
+++ wordpress-gravity-forms-uploads-attachments.php
@@ -10,7 +10,7 @@
         License:     GPL-2.0+
         License URI: http://www.gnu.org/licenses/gpl-2.0.txt
         Text Domain: wordpress-gravity-forms-uploads-attachments
-        GitHub Plugin URI: https://github.com/nevma/wordpress-gravity-forms-uploads-attachments/
+        GitHub Plugin URI: https://github.com/nevma/wordpress-gravity-forms-uploads-attachments
     */
 
 
