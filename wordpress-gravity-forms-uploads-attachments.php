b70a220660cba8340186f8b7f9b631c07e0d9830
Takis Bouyouris <takis@nevma.gr>
2017-10-20 19:00:36 +0300

    Just a title change.

--- wordpress-gravity-forms-uploads-attachments.php
+++ wordpress-gravity-forms-uploads-attachments.php
@@ -1,7 +1,7 @@
 <?php
 
     /*
-        Plugin Name: WordPress Gravity Forms uploads attachments
+        Plugin Name: Gravity Forms uploads attachments
         Plugin URI:  https://github.com/nevma/wordpress-gravity-forms-uploads-attachments
         Description: This is a short description of what the plugin does. It's displayed in the WordPress admin area.
         Version:     0.9.1
