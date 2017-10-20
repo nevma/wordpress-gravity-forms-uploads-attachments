c37a0f532e2d592d59d2301d22e694ab1d679c52
Takis Bouyouris <takis@nevma.gr>
2017-10-20 18:54:54 +0300

    Removing extraneous directories.

---
+++ wordpress-gravity-forms-uploads-attachments.php
@@ -0,0 +1,42 @@
+<?php
+
+    /*
+        Plugin Name: WordPress Gravity Forms uploads attachments
+        Plugin URI:  https://github.com/nevma/wordpress-gravity-forms-uploads-attachments
+        Description: This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+        Version:     0.9
+        Author:      Nevma.gr
+        Author URI:  https://github.com/nevma/
+        License:     GPL-2.0+
+        License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+        Text Domain: wordpress-gravity-forms-uploads-attachments
+        GitHub Plugin URI: https://github.com/nevma/wordpress-gravity-forms-uploads-attachments/trunk
+    */
+
+
+
+    /**
+     * Copyright 2015 Nevma (nevma.gr, info@nevma.gr)
+     *
+     * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General 
+     * Public License, version 2, as published by the Free Software Foundation. This program is distributed in the hope
+     * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+     * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License along with this program; if not, write to the 
+     * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA.
+     * 
+     * GPL: http://www.gnu.org/copyleft/gpl.html
+     */
+
+
+
+    if ( ! defined( 'WPINC' ) ) {
+    	die;
+    }
+
+    if ( ! defined( 'ABSPATH' ) ) {
+        die;
+    }
+
+?>
